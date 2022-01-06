8d22644712857251d945cb9771a18b2e1f7ad68a
hyuns66 <gustjr0606@naver.com>
2022-01-06 16:21:41 +0900

    initial commit

---
+++ app/src/main/java/com/likefirst/btos/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.likefirst.btos.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.likefirst.btos.R
+import com.likefirst.btos.databinding.ActivityMainBinding
+import com.likefirst.btos.ui.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    override fun initAfterBinding() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/likefirst/btos/utils/SharedPreferencesManager.kt
@@ -0,0 +1,13 @@
+package com.likefirst.btos.utils
+
+import com.likefirst.btos.ApplicationClass.Companion.X_ACCESS_TOKEN
+import com.likefirst.btos.ApplicationClass.Companion.mSharedPreferences
+
+fun saveJwt(jwtToken: String) {
+    val editor = mSharedPreferences.edit()
+    editor.putString(X_ACCESS_TOKEN, jwtToken)
+
+    editor.apply()
+}
+
+fun getJwt(): String? = mSharedPreferences.getString(X_ACCESS_TOKEN, null)
