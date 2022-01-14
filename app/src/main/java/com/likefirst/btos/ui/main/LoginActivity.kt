bac7a509c61b14a8e755db1eb400a512d454773e
mimwin <kms105903@naver.com>
2022-01-15 04:05:36 +0900

    Fix: LoginActivity, SplashActivity drawable
    해상도별 이미지를 drawable로 위치를 바꾼 후 수정된 xml

--- app/src/main/java/com/likefirst/btos/ui/main/LoginActivity.kt
+++ app/src/main/java/com/likefirst/btos/ui/main/LoginActivity.kt
@@ -1,10 +1,17 @@
 package com.likefirst.btos.ui.main
 
+import android.content.Intent
 import com.likefirst.btos.databinding.ActivityLoginBinding
 import com.likefirst.btos.ui.BaseActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate){
     override fun initAfterBinding() {
+
+        binding.loginGoogleloginTv.setOnClickListener {
+            val intent = Intent(this,OnboardingActivity::class.java)
+            finish()
+            startActivity(intent)
+        }
 
     }
 }
