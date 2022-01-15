8724c27d1edc77a69829d7510d3f9e6b679f96e2
HyomK <qet557@gmail.com>
2022-01-16 01:04:34 +0900

    Update: WrietFragment set Background

--- app/src/main/java/com/likefirst/btos/ui/main/WriteMailFragment.kt
+++ app/src/main/java/com/likefirst/btos/ui/main/WriteMailFragment.kt
@@ -19,7 +19,7 @@ class WriteMailFragment:BaseFragment<FragmentMailWriteBinding>(FragmentMailWrite
     override fun initAfterBinding() {
 
         val adapter = ArrayAdapter.createFromResource(
-            requireContext(), R.array.report_items, android.R.layout.simple_spinner_dropdown_item
+            requireContext(), R.array.delete_items, android.R.layout.simple_spinner_dropdown_item
         )
 
         binding.reportMenuList?.setAdapter(adapter)
@@ -61,42 +61,6 @@ class WriteMailFragment:BaseFragment<FragmentMailWriteBinding>(FragmentMailWrite
                     })
                     dialog.show(mActivity.supportFragmentManager, "CustomDialog")
                 }
-                //신고
-                1 -> {
-                    mActivity.changeFragment().moveFragment(R.id.letterview_main_layout,ReportFragment())
-                }
-                //차단
-                2 -> {
-                    val btn= arrayOf("취소","차단")
-                    dialog.arguments= bundleOf(
-                        "bodyContext" to "정말로 차단할까요?\n해당 발신인의 편지를 받지 않게 됩니다",
-                        "btnData" to btn
-                    )
-                    // 버튼 클릭 이벤트 설정
-                    dialog.setButtonClickListener(object: CustomDialogFragment.OnButtonClickListener{
-                        override fun onButton1Clicked() {
-                        }
-                        override fun onButton2Clicked() {
-                            val checkDialog = CustomDialogFragment()
-                            val Checkbtn= arrayOf("확인")
-                            checkDialog .arguments= bundleOf(
-                                "bodyContext" to "이제 해당인으로부터의 편지를 수신하지 않습니다.\n설정탭에서 차단해제가 가능합니다",
-                                "btnData" to Checkbtn
-                            )
-                            // 버튼 클릭 이벤트 설정
-                            checkDialog.setButtonClickListener(object: CustomDialogFragment.OnButtonClickListener{
-                                override fun onButton1Clicked() {
-                                }
-                                override fun onButton2Clicked() {
-                                }
-                            })
-                            checkDialog.show(mActivity.supportFragmentManager, "CustomDialog")
-                        }
-                    })
-                    dialog.show(mActivity.supportFragmentManager, "CustomDialog")
-
-                }
-                //일치하는게 없는 경우
 
             }
 
