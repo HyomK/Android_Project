c229388ccb717b8001e196bd7518296feae8b80c
mimwin <kms105903@naver.com>
2022-01-15 21:15:27 +0900

    Feat: MainActivity BottomNavigation
    menu selector 수정, menu Icon default color null

--- app/src/main/java/com/likefirst/btos/ui/main/MainActivity.kt
+++ app/src/main/java/com/likefirst/btos/ui/main/MainActivity.kt
@@ -6,7 +6,7 @@ import com.likefirst.btos.ui.BaseActivity
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     override fun initAfterBinding() {
-
+        binding.mainBnv.itemIconTintList = null
     }
 
 }
