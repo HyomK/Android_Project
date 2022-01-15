1cf1384d646039c096f53f62f1ae280f10b6aa95
mimwin <kms105903@naver.com>
2022-01-15 20:04:47 +0900

    Feat: MainActivity BottomNavigation
    menuItem 이름 수정, selector 수정, menuTitle enable

--- app/src/main/java/com/likefirst/btos/ui/main/MainActivity.kt
+++ app/src/main/java/com/likefirst/btos/ui/main/MainActivity.kt
@@ -1,32 +1,12 @@
 package com.likefirst.btos.ui.main
 
-import android.util.Log
-import android.view.View
-import android.widget.TextView
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
-import com.likefirst.btos.R
 import com.likefirst.btos.databinding.ActivityMainBinding
 import com.likefirst.btos.ui.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     override fun initAfterBinding() {
-        menuView()
+
     }
 
-    private fun menuView() {
-        val menuview = binding.mainBnv as BottomNavigationMenuView
-        try{
-            for (i in 0 until menuview.childCount){
-                val menuItemView = menuview.getChildAt(i) as BottomNavigationItemView
-                val title = menuItemView.findViewById(R.id.smallLabel) as TextView
-                title.visibility = View.INVISIBLE
-            }
-        }catch(e: NoSuchFieldException) {
-            Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field")
-        } catch (e: IllegalAccessException) {
-            Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode")
-        }
-    }
 }
