480949cd29561179689d059b22739953b2bbfd3e
mimwin <kms105903@naver.com>
2022-01-20 19:06:41 +0900

    Feat: Google Login using API
    구글 API 활용하여 구글 소셜 로그인 구현, login 화면에서 구글 로그인이 진행되고 이메일을 입력받는다.

--- app/src/main/java/com/likefirst/btos/ui/main/LoginActivity.kt
+++ app/src/main/java/com/likefirst/btos/ui/main/LoginActivity.kt
@@ -1,17 +1,76 @@
 package com.likefirst.btos.ui.main
 
 import android.content.Intent
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.internal.OnConnectionFailedListener
+import com.google.android.gms.tasks.Task
 import com.likefirst.btos.databinding.ActivityLoginBinding
 import com.likefirst.btos.ui.BaseActivity
 
-class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate){
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate), OnConnectionFailedListener {
+    val G_SIGN_IN : Int = 1
+
+
     override fun initAfterBinding() {
 
-        binding.loginGoogleloginTv.setOnClickListener {
-            val intent = Intent(this,OnboardingActivity::class.java)
+        lateinit var googleSignInClient: GoogleSignInClient
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+//        binding.loginGoogleLoginTv.setSize(SignInButton.SIZE_STANDARD)
+
+
+        binding.loginGoogleLoginTv.setOnClickListener{
+            var signInIntent : Intent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, G_SIGN_IN)
+        }
+        
+        //자동로그인
+//        val account = GoogleSignIn.getLastSignedInAccount(this)
+//        if(account==null){
+//            //자동 로그인 X
+//        }else{
+//            //자동 로그인
+//        }
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == G_SIGN_IN){
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+            val intent = Intent(this, OnboardingActivity::class.java)
             finish()
             startActivity(intent)
         }
+    }
 
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+        try{
+            val account = task.getResult(ApiException::class.java)
+            val email = account?.email.toString()
+            val familyName = account?.familyName.toString()
+            val givenName = account?.givenName.toString()
+            val displayName = account?.displayName.toString()
+
+            Log.d("account", email)
+            Log.d("account", familyName)
+            Log.d("account", givenName)
+            Log.d("account", displayName)
+        }catch (e: ApiException){
+            Log.w("Failed AT ", "signInResult:failed code=" + e.statusCode)
+        }
     }
 }
