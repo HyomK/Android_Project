f2570ebe974a3108082f767d06642a0fa09918d1
HyomK <qet557@gmail.com>
2022-01-29 18:21:46 +0900

    feat: UserTable

--- app/src/main/java/com/likefirst/btos/data/entities/User.kt
+++ app/src/main/java/com/likefirst/btos/data/entities/User.kt
@@ -1,11 +1,16 @@
 package com.likefirst.btos.data.entities
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
-class User (
+@Parcelize
+@Entity(tableName = "UserTable")
+data class User (
     @PrimaryKey(autoGenerate = false)
     @ColumnInfo(name = "email") var email: String= "",
     @ColumnInfo(name = "nickName") var nickName: String="",
     @ColumnInfo(name = "birth") var birth: Int = 0
-)
+): Parcelable
